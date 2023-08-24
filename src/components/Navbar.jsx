f2d1e2f21799c58ae6c52761836a228bc5871a62
rubuz <rubuz23@gmail.com>
2023-08-24 13:16:03 +0200

    desktop done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,46 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./navbar.css";
 import NavbarFeaturesDown from "./NavbarFeaturesDown";
 import NavbarCompanyDown from "./NavbarCompanyDown";
 
 const Navbar = () => {
+  const [dropdownFeatures, setDropdownFeatures] = useState(false);
+  const [dropdownCompany, setDropdownCompany] = useState(false);
+
+  const dropdownFeaturesRef = useRef(null);
+  const dropdownCompanyRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (
+        dropdownFeaturesRef.current &&
+        !dropdownFeaturesRef.current.contains(event.target)
+      ) {
+        setDropdownFeatures(false);
+      }
+      if (
+        dropdownCompanyRef.current &&
+        !dropdownCompanyRef.current.contains(event.target)
+      ) {
+        setDropdownCompany(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
     <div className="mr-auto">
       <nav>
         <ul className="flex gap-12">
-          <li className="flex items-center justify-between gap-2 relative dropdown-link">
+          <li
+            className="flex items-center justify-between gap-2 relative dropdown-link"
+            onClick={() => setDropdownFeatures(!dropdownFeatures)}
+          >
             <a href="#" className="hover:text-almost-black duration-200 ">
               Features
             </a>
@@ -21,11 +53,29 @@ const Navbar = () => {
                 d="m1 1 4 4 4-4"
               />
             </svg>
-            <div className="absolute top-10 right-0">
+            {/* {dropdownFeatures && (
+              <div
+                className="absolute top-10 right-0"
+                ref={dropdownFeaturesRef}
+              >
+                <NavbarFeaturesDown />
+              </div>
+            )} */}
+            <div
+              className={
+                dropdownFeatures
+                  ? "absolute top-10 right-0 scale-100 duration-100"
+                  : "absolute top-10 right-0 scale-0 duration-100"
+              }
+              ref={dropdownFeaturesRef}
+            >
               <NavbarFeaturesDown />
             </div>
           </li>
-          <li className="flex items-center justify-between gap-2 relative dropdown-link">
+          <li
+            className="flex items-center justify-between gap-2 relative dropdown-link"
+            onClick={() => setDropdownCompany(!dropdownCompany)}
+          >
             <a href="#" className="hover:text-almost-black duration-200 ">
               Company
             </a>
@@ -38,7 +88,19 @@ const Navbar = () => {
                 d="m1 1 4 4 4-4"
               />
             </svg>
-            <div className="absolute top-10 right-0">
+            {/* {dropdownCompany && (
+              <div className="absolute top-10 right-0" ref={dropdownCompanyRef}>
+                <NavbarCompanyDown />
+              </div>
+            )} */}
+            <div
+              className={
+                dropdownCompany
+                  ? "absolute top-10 right-0 scale-100 duration-100"
+                  : "absolute top-10 right-0 scale-0 duration-100"
+              }
+              ref={dropdownCompanyRef}
+            >
               <NavbarCompanyDown />
             </div>
           </li>
