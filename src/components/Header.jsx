6a5e361829a1357841391837308cca54ce6c2854
rubuz <rubuz23@gmail.com>
2023-08-24 14:27:25 +0200

    mobile hero done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "../assets/logo.svg";
 import Navbar from "./Navbar";
+import NavbarMobile from "./NavbarMobile";
 
 const Header = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => setWindowWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div className="max-w-[1440px] mx-auto p-6 flex justify-between items-center text-medium-gray mb-7 gap-16">
       <div>
         <img src={logo} alt="logo" />
       </div>
-      <Navbar />
-      <div>
-        <a href="#" className="hover:text-almost-black duration-200">
-          Login
-        </a>
-        <button className="ml-10 border-2 py-2 px-7 rounded-xl hover:text-almost-black hover:border-almost-black duration-200">
-          Register
-        </button>
-      </div>
+      {windowWidth < 1024 ? (
+        <NavbarMobile />
+      ) : (
+        <React.Fragment>
+          <Navbar />
+          <div>
+            <a href="#" className="hover:text-almost-black duration-200">
+              Login
+            </a>
+            <button className="ml-10 border-2 py-2 px-7 rounded-xl hover:text-almost-black hover:border-almost-black duration-200">
+              Register
+            </button>
+          </div>
+        </React.Fragment>
+      )}
     </div>
   );
 };
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import databiz from "../assets/client-databiz.svg";
 import audiophile from "../assets/client-audiophile.svg";
 import meet from "../assets/client-meet.svg";
 import maker from "../assets/client-maker.svg";
 import heroPicture from "../assets/image-hero-desktop.png";
+import heroPictureMobile from "../assets/image-hero-mobile.png";
 
 const Hero = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => setWindowWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
-    <div className="max-w-[1150px] p-6 mx-auto grid grid-cols-2 gap-32 items-end justify-center">
-      <div>
-        <div className="flex flex-col items-start gap-8">
-          <h1 className="text-almost-black font-bold text-[4rem] leading-none ">
+    <div className="max-w-[1150px] p-0 lg:p-6 mx-auto grid lg:grid-cols-2 gap-10 lg:gap-32 items-end justify-center">
+      <div className="order-2 lg:order-1">
+        <div className="flex flex-col items-center lg:items-start gap-4 lg:gap-8">
+          <h1 className="text-almost-black font-bold text-4xl lg:text-[4rem] leading-none text-center lg:text-left ">
             Make remote work
           </h1>
-          <p className="text-medium-gray max-w-[90%]">
+          <p className="text-medium-gray max-w-[90%] text-center lg:text-left">
             Get your team in sync, no matter your location. Streamline
             processes, create team rituals, and watch productivity soar.
           </p>
@@ -21,18 +33,25 @@ const Hero = () => {
             Learn More
           </button>
         </div>
-        <div className="flex items-center justify-start mt-28 gap-7">
+        <div className="flex items-center justify-between mt-10 mx-2 lg:mt-28 gap-7">
           <img src={databiz} alt="databiz logo" />
           <img src={audiophile} alt="audiophile logo" />
           <img src={meet} alt="meet logo" />
           <img src={maker} alt="maker logo" />
         </div>
       </div>
-      <div className="self-center">
-        <img
-          src={heroPicture}
-          alt="hero picture with shapes and man standing with laptop"
-        />
+      <div className="self-center lg:order-2">
+        {windowWidth < 1024 ? (
+          <img
+            src={heroPictureMobile}
+            alt="hero picture with shapes and man standing with laptop"
+          />
+        ) : (
+          <img
+            src={heroPicture}
+            alt="hero picture with shapes and man standing with laptop"
+          />
+        )}
       </div>
     </div>
   );
